e6b9b6068a2212990a0dfc16a217049e997b1d23
Muhammad Idrees Shaikh <idreesiqbal104@gmail.com>
2024-08-27 13:22:39 +0500

    homepage in main-heading section design completed

--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -3,12 +3,14 @@ import "../../assets/css/home.css";
 const Home = () => {
   return (
     <div>
-    {/* main-heading */}
+      {/* main-heading */}
       <div className="main-heading">
         <div className="container-fluid">
           <div className="row">
             <div className="col-12">
-              <h1>HORIZON <span>STUDIO</span></h1>
+              <h1>
+                HORIZON <span>STUDIO</span>
+              </h1>
               <p>LOS ANGELES,CA.</p>
             </div>
           </div>
@@ -19,13 +21,17 @@ const Home = () => {
       <div className="slider">
         <div className="container">
           <div className="row">
-            <div className="slider-text">
-              <div className="inverted-comma"><h1>"</h1></div>
-              <div className="text">
-                <p>Experience the excellence of health services with our renowned
-                specialists, comprehensive diagnostics and collaborative care.</p>
+              <div className="slider-text">
+                    <div className="inverted-comma">
+                      <h1>"</h1>
+                    </div>
+                    <div className="text">
+                      <p>Experience the excellence of health services with our
+                        renowned specialists, comprehensive diagnostics and
+                        collaborative care.</p>
+                        <h4>M. Awais</h4>
+                    </div>
               </div>
-            </div>
           </div>
         </div>
       </div>
